1950fc2526154962cf3ea4354c34a9014807d543
VGC System <vgc-system@livesystem.org>
2026-07-25 15:29:57 +0400

    test

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-﻿"use strict";
+"use strict";
 
 const stateUrl = `data/state.json?v=${Date.now()}`;
 
--- scripts/update_predictions.py
+++ scripts/update_predictions.py
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env python3
+#!/usr/bin/env python3
 """
 AI Football Lab
 Автоматическое получение матчей, подготовка статистики,
